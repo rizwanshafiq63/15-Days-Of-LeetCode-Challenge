b33bef3299c6483308030dc3becfe854e45dadf2
Muhammad Rizwan Shafiq <167992476+rizwanshafiq63@users.noreply.github.com>
2025-11-20 20:59:48 +0500

    Time: 7 ms (65.24%) | Memory: 50 MB (9.60%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -3,18 +3,11 @@ class Solution {
         HashMap<String, List<String>> map = new HashMap<>();
 
         for (String s : strs) {
-            int[] count = new int[26];
-            for (char c : s.toCharArray()) {
-                count[c - 'a']++;
-            }
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
 
-            // Convert to key string like "1#0#0#2#..."
-            StringBuilder key = new StringBuilder();
-            for (int i = 0; i < 26; i++) {
-                key.append(count[i]).append('#');
-            }
-
-            map.computeIfAbsent(key.toString(), k -> new ArrayList<>()).add(s);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
 
         return new ArrayList<>(map.values());
